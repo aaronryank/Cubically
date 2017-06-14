d7a2ed7462296e140e6a91fa330d57f25dfad189
aaronryank <rkcoder2002@gmail.com>
2017-06-14 17:44:40 +0000

    Language

--- lang.c
+++ lang.c
@@ -3,7 +3,10 @@
 #include <ctype.h>
 #include "rubiks.h"
 
-#define rubiksnotation(x) (x == 'U' ? 0 : x == 'L' ? 1 : x == 'F' ? 2 : x == 'R' ? 3 : x == 'B' ? 4 : x == 'D' ? 5 : -1)
+int mem;
+
+int rubiksnotation(char);
+int execute(int,int);
 
 int main(int argc, char **argv)
 {
@@ -11,20 +14,79 @@ int main(int argc, char **argv)
 
     FILE *in = argc > 2 ? fopen(argv[1],"r") : stdin;
 
-    int loop = 1;
+    int loop = 1, command;
     while (loop) {
-        int face  = getc(in);
-        int turns = getc(in);
-
-        if (rubiksnotation(face)+1) {
-            face = rubiksnotation(face);
-            turns = (turns == '\'') ? 3 : turns - '0';
-            turncube(face,turns);
-        } else if (face == 'E' || face == -1) {
+        int c = getc(in);
+        if (c == EOF)
             loop = 0;
-        }
+        else if (isdigit(c))
+            loop = execute(command,c - '0');
+        else
+            command = c;
     }
 
-    putchar('\n');
+    printf("\nMemory: %d\n\n",mem);
     printcube();
+}
+
+#define faceval (cube[face][0][0] + cube[face][0][1] + cube[face][0][2] + cube[face][1][0] + cube[face][1][1] + cube[face][1][2] + cube[face][2][0] + cube[face][2][1] + cube[face][2][2])
+
+int execute(int command, int argument)
+{
+     if (rubiksnotation(command)+1) {
+        int face  = rubiksnotation(command);
+        int turns = argument;
+        turncube(face,turns);
+    }
+    else if (command == '+') {
+        int face = argument;
+        mem += faceval;
+    }
+    else if (command == '-') {
+        int face = argument;
+        mem -= faceval;
+    }
+    else if (command == '/') {
+        int face = argument;
+        mem /= faceval;
+    }
+    else if (command == '*') {
+        int face = argument;
+        mem *= faceval;
+    }
+    else if (command == '=') {
+        int face = argument;
+        mem = (mem == faceval);
+    }
+    else if (command == '%') {
+        int face = argument;
+        if (face == 6)
+            printf("%d",mem);
+        else
+            printf("%d",faceval);
+    }
+    else if (command == '@') {
+        int face = argument;
+        if (face == 6)
+            putchar(mem);
+        else
+            putchar(faceval);
+    }
+    else if (command == 'E') {
+        return 0;
+    }
+    return 1;
+}
+
+int rubiksnotation(char x)
+{
+    switch (x) {
+      case 'U': return 0;
+      case 'L': return 1;
+      case 'F': return 2;
+      case 'R': return 3;
+      case 'B': return 4;
+      case 'D': return 5;
+      default:  return -1;
+    }
 }
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 all:
-	gcc rubiks.c stdin.c visualizer.c -o visualizer
-	gcc rubiks.c lang.c -o rubiks-lang
+	gcc -g rubiks.c stdin.c visualizer.c -o visualizer
+	gcc -g rubiks.c lang.c -o rubiks-lang
